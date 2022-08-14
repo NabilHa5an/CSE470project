f99a033da57394d5e8ab1df16398ad5354c960f2
NabilHasan1123 <nabil.hasan1999@gmail.com>
2022-08-14 23:47:24 +0600

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+@section('content')
+   <div class="card">
+       <div class="card-body">
+          <h1>Nabil Hasan </h1>
+    
+       </div>
+
+   </div>
+
+@endsection
